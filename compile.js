7408d6fe00b41729a4b900c0a5e6e91e54b5623e
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-02-17 22:18:45 +0530

    add compile.js

---
+++ compile.js
@@ -0,0 +1,32 @@
+const path = require('path'); //Path acquiring irrespective of OS
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');  //Path aquiring
+
+const source = fs.readFileSync(inboxPath, 'UTF-8'); //reading the file
+
+
+var input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol': {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+};
+
+var output=JSON.parse(solc.compile(JSON.stringify(input)));
+ 
+//console.log(JSON.stringify(output,null,'\t')); //Pretty Console Output
+
+console.log(output.contracts['Inbox.sol']);
+
+module.exports=output.contracts['Inbox.sol'];
